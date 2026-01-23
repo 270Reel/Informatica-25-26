76176c886fc09508be6c05d10e1e611fdc347b34
Andrea <168139791+270Reel@users.noreply.github.com>
2026-01-23 11:56:30 +0000

    update

--- INF/Teoria/ProgettoScuola/inserisci.php
+++ INF/Teoria/ProgettoScuola/inserisci.php
@@ -1,28 +1,26 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "gestionestudenti");
 
+
 if ($conn->connect_error) {
-    die("Connessione fallita");
+    die("Connessione fallita: " . $conn->connect_error);
 }
 
-if (isset($_POST['salva'])) {
-    $nome = $_POST['nome'];
-    $cognome = $_POST['cognome'];
-    
-    $sql = "INSERT INTO studenti (nome, cognome)
-            VALUES ('$nome', '$cognome')";
-    $conn->query($sql);
-    echo "Studente inserito con successo!";
+$nome = $_POST['nome'];
+$cognome = $_POST['cognome'];
+
+$sql = "INSERT INTO studenti (nome, cognome) VALUES (?, ?)";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->bind_param("ss", $nome, $cognome);
+
+if ($stmt->execute()) {
+    echo "Studente inserito con successo! ID generato: " . $stmt->insert_id;
+} else {
+    echo "Errore durante l'inserimento: " . $stmt->error;
 }
+
+$stmt->close();
+$conn->close();
 ?>
-
-<h2>Inserimento Studente</h2>
-
-<form method="post">
-    Nome: <input type="text" name="nome" required><br><br>
-    Cognome: <input type="text" name="cognome" required><br><br
-    <input type="submit" name="salva" value="Inserisci">
-</form>
-
-<br>
-<a href="index.html">Torna al menu</a>
