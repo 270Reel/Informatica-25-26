38edfb743f4f7e251b4e2b7bfe9f88d4e83bf5d4
Andrea <168139791+270Reel@users.noreply.github.com>
2026-02-26 10:18:50 +0000

    update

--- INF/Laboratorio/ProgettoScuola/php/dashboardstudenti.php
+++ INF/Laboratorio/ProgettoScuola/php/dashboardstudenti.php
@@ -36,7 +36,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Studenti</title>
-    <link rel="stylesheet" href="ProgettoScuola/css/stiledashboard.css">
+    <link rel="stylesheet" href="css/stiledashboard.css">
 </head>
 
 <body>
